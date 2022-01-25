621c79d41b1400720a990d6e3e95167277bf3e65
MAR_Makarov <aelhasbi11@gmail.com>
2022-01-25 18:00:12 +0100

    Just placed template

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+namespace App\Http\Controllers;
+use App\View\Components\Alert;
+use Illuminate\Routing\Controller as BaseController;
+use App\Http\Controllers\Controller;
+
+class IndexController extends BaseController
+{
+
+
+    public function index() {
+        return view('layouts.main');
+    }
+}
