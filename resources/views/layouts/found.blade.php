d12411894bf8e4ad15a058af8985735b9cfb402d
MAR_Makarov <aelhasbi11@gmail.com>
2022-01-26 17:44:56 +0100

    Dashboard
    first commit fixed hrefs

--- resources/views/layouts/found.blade.php
+++ resources/views/layouts/found.blade.php
@@ -1,9 +1,11 @@
 @extends('layouts.main')
 @section('title', 'found')
+
+
+
 @section('main-content')
     <div class="root" style="margin-top: 200px">
         <div class="results-container d-flex flex-column justify-content-center">
       </div>
     </div>
 @endsection
->>>>>>> 27c27ccfcc48b551fb0eec56593e792e34de474d
