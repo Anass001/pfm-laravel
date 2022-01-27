81edf21c78f23acd2abadce34a959384dc507544
MAR_Makarov <aelhasbi11@gmail.com>
2022-01-27 19:42:02 +0100

    fixed a bunch of hrefs

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -16,12 +16,12 @@
     <!-- load stylesheets -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
     <!-- Google web font "Open Sans" -->
-    <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">                <!-- Font Awesome -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">                                      <!-- Bootstrap style -->
-    <link rel="stylesheet" type="text/css" href="slick/slick.css"/>
-    <link rel="stylesheet" type="text/css" href="slick/slick-theme.css"/>
-    <link rel="stylesheet" type="text/css" href="css/datepicker.css"/>
-    <link rel="stylesheet" href="css/tooplate-style.css">                                   <!-- Templatemo style -->
+    <link rel="stylesheet" href="/font-awesome-4.7.0/css/font-awesome.min.css">                <!-- Font Awesome -->
+    <link rel="stylesheet" href="/css/bootstrap.min.css">                                      <!-- Bootstrap style -->
+    <link rel="stylesheet" type="text/css" href="/slick/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="/slick/slick-theme.css"/>
+    <link rel="stylesheet" type="text/css" href="/css/datepicker.css"/>
+    <link rel="stylesheet" href="/css/tooplate-style.css">                                   <!-- Templatemo style -->
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -39,7 +39,7 @@
         <div class="container">
             <div class="row">
                 <nav class="navbar navbar-expand-lg narbar-light">
-                    <a class="navbar-brand mr-auto" href="#">BookingManager</a>
+                    <a class="navbar-brand mr-auto" href="/">BookingManager</a>
                     <button type="button" id="nav-toggle" class="navbar-toggler collapsed" data-toggle="collapse"
                             data-target="#mainNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
@@ -70,10 +70,9 @@
         @yield('main-content')
     </div>
 
-    <footer>
-        <div
-            style="display: flex; position:relative;width: 100%; height: 50px; bottom: 0;flex-direction:row;justify-content:center;background-color: #003580 ;color: white !important">
-            <p style="margin: auto;">
+    <footer style="display: flex; position:relative ;width: 100%; height: 50px; bottom: 0;flex-direction:row;justify-content:center;background-color: #003580 ;color: white !important">
+        <div>
+            <p style="margin:auto" >
                 Copyright &copy; <span class="tm-current-year">2018</span> Your Company
 
                 - Design: Tooplate</p>
@@ -82,13 +81,13 @@
 </div>
 
 <!-- load JS files -->
-<script src="js/jquery-1.11.3.min.js"></script>             <!-- jQuery (https://jquery.com/download/) -->
-<script src="js/popper.min.js"></script>                    <!-- https://popper.js.org/ -->
-<script src="js/bootstrap.min.js"></script>                 <!-- https://getbootstrap.com/ -->
-<script src="js/datepicker.min.js"></script>                <!-- https://github.com/qodesmith/datepicker -->
-<script src="js/jquery.singlePageNav.min.js"></script>
+<script src="/js/jquery-1.11.3.min.js"></script>             <!-- jQuery (https://jquery.com/download/) -->
+<script src="/js/popper.min.js"></script>                    <!-- https://popper.js.org/ -->
+<script src="/js/bootstrap.min.js"></script>                 <!-- https://getbootstrap.com/ -->
+<script src="/js/datepicker.min.js"></script>                <!-- https://github.com/qodesmith/datepicker -->
+<script src="/js/jquery.singlePageNav.min.js"></script>
 <!-- Single Page Nav (https://github.com/ChrisWojcik/single-page-nav) -->
-<script src="slick/slick.min.js"></script>                  <!-- http://kenwheeler.github.io/slick/ -->
+<script src="/slick/slick.min.js"></script>                  <!-- http://kenwheeler.github.io/slick/ -->
 <script>
 
     /* Google map
